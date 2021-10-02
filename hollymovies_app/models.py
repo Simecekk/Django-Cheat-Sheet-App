200d78a555baf41e6c9ae6c17892e0d0dc6c5bce
David Simek <david.simek1998@gmail.com>
2021-10-02 13:51:42 +0200

    added M2M to gernes

--- hollymovies_app/models.py
+++ hollymovies_app/models.py
@@ -13,11 +13,15 @@ class Genre(models.Model):
     def is_genre_horror(self) -> bool:
         return self.name == self.HORROR
 
+    def __str__(self):
+        return f'{self.get_name_display()} : {self.id}'
+
 
 class Movie(models.Model):
     name = models.CharField(max_length=512)
     likes = models.IntegerField(default=0)
     description = models.TextField(blank=True, default='')
+    genres = models.ManyToManyField(Genre, related_name='movies')
 
     def __str__(self):
         return f'{self.name} : {self.id}'
