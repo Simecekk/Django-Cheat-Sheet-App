88a7b78a349f2d60e2dad75b2de87b84e6e68564
David Simek <david.simek1998@gmail.com>
2021-10-10 11:57:39 +0200

    additonal field in form

--- hollymovies_app/forms.py
+++ hollymovies_app/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 
+from hollymovies_app.models import Movie
+
 
 class ContactForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    movies = forms.ModelMultipleChoiceField(queryset=Movie.objects.all())
     subject = forms.CharField(required=False)
+    contact_at = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
     description = forms.CharField(widget=forms.Textarea)
